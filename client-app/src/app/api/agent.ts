1f4f670c49704ebfa5d7bfd84fb4879ec5f66a2a
grubi97 <grubesa.marko97@gmail.com>
2021-05-17 19:56:32 +0200

    add reg and login on client

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -83,7 +83,7 @@ const Activities = {
 const Account = {
   current: () => requests.get<User>("/account"),
   login: (user: UserFormValues) => requests.post<User>("/account/login", user),
-  register:(user:UserFormValues)=>requests.post<User>('/account/register',user)
+  register:(user:UserFormValues)=>requests.post<User>('/account/register',user),
 };
 
 const agent = {
